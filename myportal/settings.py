2cf777a9a3f45ebc043df852d181946825b36a72
ragul <ragulraja893@gmail.com>
2024-05-17 17:52:07 +0530

    seconhb

--- myportal/settings.py
+++ myportal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,19 @@ WSGI_APPLICATION = 'myportal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://jobmatrix_owner:0wD5tnmPdBXF@ep-small-cloud-a1wby35h.ap-southeast-1.aws.neon.tech/jobmatrix?sslmode=require',
+        conn_max_age=600
+    )
 }
+
 
 
 
